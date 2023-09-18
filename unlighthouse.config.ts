da68e242450da469e312202d70a6c8a46a9757d3
PurkeGopinand97 <139464116+PurkeGopinand97@users.noreply.github.com>
2023-09-18 15:22:11 +0530

    Update unlighthouse.config.ts

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -2,9 +2,9 @@ export default {
   scanner: {
     maxRoutes: 200,
   },
-  lighthouseOptions: {
-    // ['performance', 'best-practices', 'accessibility', 'seo', 'pwa'],
-    onlyCategories: ['best-practices', 'accessibility', 'seo', 'pwa'],
-  },
+  // lighthouseOptions: {
+  //   // ['performance', 'best-practices', 'accessibility', 'seo', 'pwa'],
+  //   onlyCategories: ['best-practices', 'accessibility', 'seo', 'pwa'],
+  // },
   debug: false,
 }
